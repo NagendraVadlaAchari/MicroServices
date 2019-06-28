e0ff719a659365413ea0b505ad9d0ab826ce8c2b
NagendraVadlaAchari <49055147+NagendraVadlaAchari@users.noreply.github.com>
2019-06-28 17:25:59 +0530

    Add files via upload

---
+++ MathFunctions/src/com/mits/mathfnxs/MathFunctions1.java
@@ -0,0 +1,18 @@
+package com.mits.mathfnxs;
+
+public class MathFunctions1 {
+	
+	public static void main(String[] args) {
+		double d=-100.6;
+		//float f=-120.3f;
+		System.out.println(Math.abs(d));
+		System.out.println(Math.ceil(d));
+		System.out.println(Math.floor(d));
+		System.out.println(Math.round(d));
+		System.out.println(Math.max(10, 20));
+		System.out.println(Math.min(1.5, 1.0));
+		System.out.println(Math.pow(2, 2));
+		
+	}
+
+}
